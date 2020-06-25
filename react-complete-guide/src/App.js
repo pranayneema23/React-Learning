2c311cd564a8948e4c9c6550b0231ef5331547d8
pneema3 <pneema3@dxc.com>
2020-06-25 21:46:48 +0530

    Understanding JSX

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
         <h1>Hi, I'm Pranay!!</h1>
       </div>
     );
+    ////return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Hi, Does it works?'));
   }
 }
 
