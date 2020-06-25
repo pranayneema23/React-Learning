136080ec42a3733d1b5650e5598c19088eecb9d2
pneema3 <pneema3@dxc.com>
2020-06-25 22:07:30 +0530

    Understanding functional component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
+//Class based, smart, stateful component
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm Pranay!!</h1>
+        <Person />
       </div>
     );
     ////return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Hi, Does it works?'));
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+//ES 6 Syntex
+//Functional, stateless, presentational, dumb components.
+const person = () => {
+    return <p>I'm a Person!</p>
+}
+
+export default person; 
