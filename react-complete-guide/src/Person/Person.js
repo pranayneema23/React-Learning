7cac2ce803088a0df4ef92bbf69af420bc9a3eb1
pneema3 <pneema3@dxc.com>
2020-06-25 22:17:59 +0530

    Outputting dynamic content

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 //ES 6 Syntex
 //Functional, stateless, presentational, dumb components.
 const person = () => {
-    return <p>I'm a Person!</p>
+return <p>I'm a Person and I'm {Math.floor(Math.random()*30)} year's old!</p>
 }
 
 export default person; 
