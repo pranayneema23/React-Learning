939cce9459a61762d0efdf80154635d18fcacf21
pneema3 <pneema3@dxc.com>
2020-06-26 22:13:55 +0530

    Understanding states

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,13 +4,22 @@ import Person from './Person/Person';
 
 //Class based, smart, stateful component
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Pranay', age: 22},
+      {name: 'Ritika', age: 19},
+      {name: 'Stephanie', age: 27}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm Pranay!!</h1>
-        <Person name='Pranay' age='22'/>
-        <Person name='Ritika' age='19'>My Hobbies : Sleeping </Person>
-        <Person name='Naman' age='24'/>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies : Sleeping </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     ////return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Hi, Does it works?'));
