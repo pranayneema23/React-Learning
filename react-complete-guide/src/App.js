20ab2bd5abda9f2f0fcba0ece6752afe2eba3dcb
pneema3 <pneema3@dxc.com>
2020-06-26 22:27:31 +0530

    Handling events with method

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -12,11 +12,15 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    console.log("Was clicked!!");
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm Pranay!!</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies : Sleeping </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
